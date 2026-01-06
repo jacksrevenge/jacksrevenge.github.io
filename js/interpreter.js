6a1c4f61d3918594f8059b31fa6160738eb63ddb
jack-blomquist-nti-johanneberg <jackblomquist@hotmail.com>
2026-01-06 16:40:00 +0100

    functionality done!!

--- js/interpreter.js
+++ js/interpreter.js
@@ -8,8 +8,31 @@ function run(event) {
     const program = document.getElementById("program").value
     const integer = parseInt(document.getElementById("integer").value)
 
-    console.log(program.split(","))
+    const split_program = program.split(",")
 
+    const clean_program = []
+
+    for (let i = 0; i < split_program.length; i++) {
+        clean_program.push(eval(split_program[i]))
+    }
+
+    let n = parseFloat(integer)
+    
+    for (let j = 0; j < clean_program.length; j++) {
+        let c = 0
+        while (Number.isInteger(n * clean_program[j])){
+            n = n * clean_program[j]
+            console.log(n)
+            c++
+
+            if (c >= 100) {
+                console.log("out of time!")
+                break
+            }
+        }
+    }
+    
+    console.log(n)
 }
 
 
